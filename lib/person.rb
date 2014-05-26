dae4d04837d751d52ef0bcc23a8a8ce59b3144f4
Thiago Borges <borges@helabs.com.br>
2014-05-25 22:53:26 -0300

    Include attribute methods for Person#name

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,23 @@
+require 'active_model'
+
 class Person
+  include ActiveModel::AttributeMethods
+  attr_accessor :name
+
+  attribute_method_suffix '_taken?'
+  define_attribute_methods %w(name)
+
   def initialize(params)
     @name = params[:name]
   end
+
+  def attribute_taken?(attr)                        # def name_taken?(name)
+    send("#{attr}_collection").include?(send(attr)) #   name_collection.include?(name)
+  end                                               # end
+
+  private
+    def name_collection
+      %w(Thiago Anézio Paulo Camila)
+    end
+
 end
