f1116dc47adaa3d548d03a0bb49bf64090914eb2
Thiago Borges <borges@helabs.com.br>
2014-05-25 22:48:14 -0300

    Failing test to check if name was taken

--- lib/person.rb
+++ lib/person.rb
@@ -1,2 +1,5 @@
 class Person
+  def initialize(params)
+    @name = params[:name]
+  end
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe Person do
+  describe 'check existent attributes' do
+    context 'name' do
+      it 'says Thiago name is already taken' do
+        person = Person.new(name: 'Thiago')
+        expect(person.name_taken?).to be_true
+      end
 
+      it 'says Gumercindo name is not taken' do
+        person = Person.new(name: 'Gumercindo')
+        expect(person.name_taken?).to be_false
+      end
+    end
+  end
 end
